8a61a41e8ca162a4efecee104b1d61a785bc7387
Rolando Morales <rolando.moralesjr21@gmail.com>
2020-12-05 21:42:02 -1000

    reference static

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv"
 import messageRoutes from './routes/messageRoutes.js'
+import path from "path"
 
 dotenv.config()
 
@@ -13,9 +14,19 @@ app.use(express.json());
 
 app.use("/api/message", messageRoutes)
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 const PORT = process.env.PORT || 5000
 
